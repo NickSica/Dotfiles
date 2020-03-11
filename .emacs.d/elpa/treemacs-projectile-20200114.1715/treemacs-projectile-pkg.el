3b34078f14eb10cd4b6143105447019da3dc38a8
NickSica <njs82@drexel.edu>
2020-03-11 17:04:32 -0500

    Update Dotfiles

---
+++ .emacs.d/elpa/treemacs-projectile-20200114.1715/treemacs-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "treemacs-projectile" "20200114.1715" "Projectile integration for treemacs" '((emacs "25.2") (projectile "0.14.0") (treemacs "0.0")) :commit "cbed83346c0b6bee22465a6ca4bad65c3239f9c9" :authors '(("Alexander Miller" . "alexanderm@web.de")) :maintainer '("Alexander Miller" . "alexanderm@web.de") :url "https://github.com/Alexander-Miller/treemacs")
